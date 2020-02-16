cecb4c88718fea0d83b9c804e75a8d6eda703a10
chithien0909 <chithien0909@gmail.com>
2020-02-16 10:06:14 +0700

    Add create and getAll User

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,34 +1,39 @@
 import 'reflect-metadata';
-import { Request, Response } from 'express';
-import { Controller, Middleware, Get, Put, Post, Delete } from '@overnightjs/core';
-import { Logger } from '@overnightjs/logger';
-import { User } from '../entities/User';
+import {BAD_REQUEST, OK} from 'http-status-codes';
+import {Request, Response} from 'express';
+import {Controller, Delete, Get, Post, Put} from '@overnightjs/core';
+import {Logger} from '@overnightjs/logger';
+import {User} from '../entities/User';
 import {getMongoManager} from 'typeorm';
+import {UserRole} from '../enums';
 
 @Controller('api/users')
 export class UserController {
     @Get('getAll')
-    private getAllUser(req: Request, res: Response) {
-        // const user: User = new User();
-        res.send('Users');
+    private async getAllUser(req: Request, res: Response) {
+        const manager = getMongoManager();
+        const users = await manager.find(User);
+        return res.status(OK).json({
+            data: users,
+        })
     }
     @Post('create')
     private async createUser(req: Request, res: Response) {
-
+        Logger.Info(req.body);
         const user: User = new User();
         user.fullname = req.body.fullname;
         user.email = req.body.email;
         user.password = req.body.password;
-        user.roles = req.body.roles;
+        user.roles = UserRole.GUEST;
         user.username = req.body.username;
         const manager = getMongoManager();
         try {
             await manager.save(user);
-            return res.status(200).json({
+            return res.status(OK).json({
                 message: 'Create user success'
             });
         } catch (e) {
-            return res.status(400).json({
+            return res.status(BAD_REQUEST).json({
                 message: 'Create failed',
                 error: e,
             });
@@ -46,6 +51,7 @@ export class UserController {
     private delMessage(req: Request, res: Response) {
         try {
             throw new Error(req.params.msg);
+
         } catch (err) {
             Logger.Err(err, true);
             return res.status(400).json({
